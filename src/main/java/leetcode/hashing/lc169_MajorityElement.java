e0d0b157c831f663922d11a0828636d6e0493e97
Ruqsana <ruqsana123begum@gmail.com>
2025-06-29 00:02:28 +0530

    Hashing leetcode problem

---
+++ src/main/java/leetcode/hashing/lc169_MajorityElement.java
@@ -0,0 +1,27 @@
+package leetcode.hashing;
+
+import java.util.HashMap;
+
+public class lc169_MajorityElement {
+
+    public static void majorityElement(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int n = nums.length;
+        for (int num : nums) {
+            if (map.containsKey(num))
+                map.put(num, map.get(num) + 1);
+            else
+                map.put(num, 1);
+        }
+
+        for (int key : map.keySet()) {
+            if (map.get(key) > n / 3)
+                System.out.println(key);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 2, 3, 1, 3, 1, 4, 1, 5, 3};
+        majorityElement(nums);
+    }
+}
