f3720a52e6401ddb323f22ce52653321ed8b1794
Ruqsana <107501777+Ruqsana2504@users.noreply.github.com>
2025-12-03 16:42:02 +0530

    Blue Yonder interview question

--- src/main/java/com/blueyonder/Interview.java
+++ src/main/java/com/blueyonder/Interview.java
@@ -16,5 +16,29 @@ public class Interview {
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
 
         System.out.println(characterFrequency);
+
+        //Convert 2D Matrix
+        int[][] mat = {
+                {1, 2, 3},    // 3 6 9
+                {4, 5, 6},  // 2 5 8
+                {7, 8, 9}   // 1 4 7
+        };
+
+        int[][] result = new int[3][3];
+
+        int rows = mat.length;
+        int cols = mat[0].length;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                result[rows - j - 1][i] = mat[i][j];
+            }
+        }
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
