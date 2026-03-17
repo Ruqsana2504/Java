4d4e50755c33cf9c5a5fdead7bc98800841ce5a6
Ruqsana <107501777+Ruqsana2504@users.noreply.github.com>
2026-03-18 01:22:45 +0530

    Implement island counting algorithm

--- src/main/java/com/wissentech/Island.java
+++ src/main/java/com/wissentech/Island.java
@@ -18,10 +18,42 @@ public class Island {
                 {0, 0, 1, 0, 1},
                 {0, 0, 0, 1, 1}
         };
+
+//        grid = new int[][]{
+//                {1, 1, 1, 1, 0},
+//                {1, 1, 0, 1, 0},
+//                {1, 1, 0, 0, 0},
+//                {0, 0, 0, 0, 0}
+//        };
         System.out.println(countIslands(grid));
     }
 
-    private static boolean countIslands(int[][] grid) {
-        return false;
+    private static int countIslands(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+        int noOfIslands = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1) {
+                    dfs(grid, i, j);
+                    noOfIslands++;
+                }
+            }
+        }
+        return noOfIslands;
+    }
+
+    private static void dfs(int[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == 0) {
+            return;
+        }
+
+        grid[i][j] = 0;
+
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
     }
 }
